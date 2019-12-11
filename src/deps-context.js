a24f8fbf02ead46a282e697c178a3b74c680dd9d
Luca Barone <baro.luc@gmail.com>
2019-12-11 22:30:17 +0100

    Add ts in js

--- src/deps-context.js
+++ src/deps-context.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
-const DepsContext = React.createContext({})
+const DepsContext = React.createContext(new Map())
 
+/**
+ * @typedef {[any, Object]} Deps
+ * @param {Object} props
+ * @param {Deps[]} props.depsMap
+ */
 export const DepsProvider = ({depsMap, ...props}) => {
   if (!depsMap) {
     throw new Error('DepsProvider is useless without a depsMap')
